d7deddbc3c8b7e4ef9735dd258a4ddc23716a0c4
filauro1989 <filauro1989@gmail.com>
2022-02-18 16:53:47 +0100

    printed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,14 @@
 @section('content')
     @foreach ($trains as $train)
     <div class="card">
+        <h1>{{ $train->stazione_di_partenza }}</h1>
+        <h1>{{ $train->stazione_di_arrivo }}</h1>
+        <h3>{{ $train->orario_di_partenza }}</h3>
+        <h3>{{ $train->orario_di_arrivo }}</h3>
+        <h3>Codice Treno: {{ $train->codice_treno }}</h3>
+        <h1>{{ $train->numero_carrozze }}</h1>
         <h1>{{ $train->data }}</h1>
+
     </div>
     @endforeach
 @endsection
