1a51a89f7e25dd199fab0d3468cd0fa0d1b048a9
filauro1989 <filauro1989@gmail.com>
2022-02-21 16:11:44 +0100

    faker done with db:seed

--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -15,12 +15,12 @@ class TrainSeeder extends Seeder
     {
         for ($i=0; $i < 200; $i++) { 
             $newTrain = new Train();
-            $newTrain->stazione_di_partenza = $faker->cityName();
-            $newTrain->stazione_di_arrivo = $faker->cityName();
+            $newTrain->stazione_di_partenza = $faker->city();
+            $newTrain->stazione_di_arrivo = $faker->city();
             $newTrain->orario_di_partenza = $faker->time();
             $newTrain->orario_di_arrivo = $faker->time();
             $newTrain->codice_treno = $faker->randomNumber(6, true);
-            $newTrain->numero_carrozze = $faker->randomNumber(2, false);
+            $newTrain->numero_carrozze = $faker->numberBetween(3, 20);
             $newTrain->in_orario = $faker->boolean();
             $newTrain->cancellato = $faker->boolean();
             $newTrain->data = $faker->dateTimeBetween('-1 week', '+1 week');
