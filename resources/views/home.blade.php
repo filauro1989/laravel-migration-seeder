4d8d04bdaa5e9b93da1a0ddff7313e7ea3e4adbe
filauro1989 <filauro1989@gmail.com>
2022-02-21 16:23:09 +0100

    printed in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,25 @@
 @endsection
 
 @section('content')
-    @foreach ($trains as $train)
-    <div class="card">
-        <h1>{{ $train->stazione_di_partenza }}</h1>
-        <h1>{{ $train->stazione_di_arrivo }}</h1>
-        <h3>{{ $train->orario_di_partenza }}</h3>
-        <h3>{{ $train->orario_di_arrivo }}</h3>
-        <h3>Codice Treno: {{ $train->codice_treno }}</h3>
-        <h1>{{ $train->numero_carrozze }}</h1>
-        <h1>{{ $train->data }}</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($trains as $train)
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h1>Partenza da: {{ $train->stazione_di_partenza }}</h1>
+                        <h1>Arrivo a: {{ $train->stazione_di_arrivo }}</h1>
+                        <h3>In partenza alle ore: {{ $train->orario_di_partenza }}</h3>
+                        <h3>In arrivo alle ore:{{ $train->orario_di_arrivo }}</h3>
+                        <h4>Codice Treno: {{ $train->codice_treno }}</h4>
+                        <h2>Numero carrozze: {{ $train->numero_carrozze }}</h2>
+                        <h2>In partenza il giorno: {{ $train->data }}</h2>
+                        <a href="{{ route('show', $train) }}" class="btn btn-primary">View Train</a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
     </div>
-    @endforeach
+    
 @endsection
